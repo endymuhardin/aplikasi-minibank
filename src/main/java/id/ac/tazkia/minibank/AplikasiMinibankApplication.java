70a926bf391101adaa3242770e36a683c2af7885
Endy Muhardin <endy.muhardin@gmail.com>
2025-07-08 15:17:35 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/minibank/AplikasiMinibankApplication.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.minibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiMinibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiMinibankApplication.class, args);
+	}
+
+}
