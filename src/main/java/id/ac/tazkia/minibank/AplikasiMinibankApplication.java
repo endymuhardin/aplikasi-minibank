1ba5e7529000ce4f1ebde12c36452529f3d38452
Endy Muhardin <endy.muhardin@gmail.com>
2025-08-10 17:50:36 +0700

    explicitly enable spring security dialect

--- src/main/java/id/ac/tazkia/minibank/AplikasiMinibankApplication.java
+++ src/main/java/id/ac/tazkia/minibank/AplikasiMinibankApplication.java
@@ -3,6 +3,7 @@ package id.ac.tazkia.minibank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
@@ -17,5 +18,10 @@ public class AplikasiMinibankApplication {
 	public LayoutDialect layoutDialect() {
 	return new LayoutDialect();
 	}
+
+	@Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
 
 }
